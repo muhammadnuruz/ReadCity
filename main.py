2ad9458adaa84b090bf29f1178d04962436ee803
muhammadnur <muhammadnurpdp@gmail.com>
2024-08-19 15:52:50 +0500

    ALl commit

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from bot.handlers import *
 
 admins = [1974800905, 93979910]
 
+
+async def on_startup(dp):
+    print("Bot is online!")
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
+    loop.run_until_complete(on_startup(dp))
     executor.start_polling(dp, skip_updates=True)
